2f76840d28b7500b1203f9886051d33dd6618c8d
f-mio <snowy.landscape.24.oct@gmail.com>
2021-09-12 15:20:36 +0900

    adminページ作成
    ・adminページの作成
    ・viewの作成(途中)

--- quiz/models.py
+++ quiz/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Quiz(models.Model):
+    quiz_title    = models.CharField(max_length=10, default="")
+    quiz_category = models.CharField(max_length=10, default="")
     quiz_text     = models.CharField(max_length=400, default="")
     quiz_correct  = models.IntegerField(default=0)
+    choices_1     = models.CharField(max_length=100, default="")
+    choices_2     = models.CharField(max_length=100, default="")
+    choices_3     = models.CharField(max_length=100, default="")
+    choices_4     = models.CharField(max_length=100, default="")
     pub_date      = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.quiz_title + " 【" + self.quiz_category +"】"
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Invitees(models.Model):
     family_name = models.CharField(max_length=10, default="")
@@ -21,3 +35,9 @@ class Invitees(models.Model):
     answer_9    = models.IntegerField(default=0)
     answer_10   = models.IntegerField(default=0)
     pub_date    = models.DateTimeField('date published')
+
+    def __str__(self):
+        return ("{} {} pont : {}".format(self.family_name, self.first_name, self.point))
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Quiz, Invitees
+
 
 def index(request):
-    return HttpResponse('Hello World!')
+    try:
+        latest_question_list = Quiz.objects.order_by('-pub_date')[:10]
+        context = {
+            'latest_question_list': latest_question_list
+        }
+    except Quiz.DoesNotExist:
+        raise Http404("Quiz does not exist.")
+    return render(request, 'quiz/index.html' ,context)
+
+def new(request):
+    return HttpResponse('answer')
+
+def results(request):
+    invitees = get_object_or_404(Invitees)
+    context = {
+        'invitees': invitees
+    }
+    return HttpResponse('result')
+#    return render(request, 'quiz/results.html', context)
