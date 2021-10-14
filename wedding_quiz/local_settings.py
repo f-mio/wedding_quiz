e2b87a761abc6ea17dd8fa09b62dd1d981482638
f-mio <snowy.landscape.24.oct@gmail.com>
2021-10-15 08:14:40 +0900

    migration

--- wedding_quiz/local_settings.py
+++ wedding_quiz/local_settings.py
@@ -7,11 +7,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wedding_quiz',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_!+3w&excto@0az!t*32yriwxpwkvq$7@=x(@4a1hvl=4rxys9'
 
--- wedding_quiz/settings.py
+++ wedding_quiz/settings.py
@@ -81,6 +81,14 @@ DATABASES = {
         'PORT': '',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'wedding_quiz',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -117,7 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# 開発時で一時的にコメントアウト
+#STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
