d8de113d235dac8d37860e9fa42e63115dc5e6b7
f-mio <snowy.landscape.24.oct@gmail.com>
2021-10-17 03:22:29 +0900

    ローカル環境のDB修正

--- wedding_quiz/settings.py
+++ wedding_quiz/settings.py
@@ -81,14 +81,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'wedding_quiz',
-#         'HOST': '',
-#         'PORT': '',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -126,11 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-
-# 開発時で一時的にコメントアウト
-#STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
